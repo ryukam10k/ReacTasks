c44e3c8617d3ac9a78119b14c4f7cfbab4005b76
ryukam10k <r.kamioka@gmail.com>
2018-05-19 11:37:10 +0900

    Componentize the title.

---
+++ src/Title.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import logo from './logo.svg';
+
+function Title() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to ReacTasks</h1>
+    </header>
+  );
+}
+
+export default Title;
